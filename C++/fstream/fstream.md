57a4c04a53f61870f3ce8ede10fd9812230b501b
周庆文 <zhouqingwen@csvw.com>
2019-11-22 15:24:23 +0800

    2019-11-22-15:24

---
+++ C++/fstream/fstream.md
@@ -0,0 +1,4 @@
+# \<fstream>
+**Input/output file stream class to operate on files.**
+
+可以向文件写入和从文件读取, 如果要按行读取文件内容还是要用getline().
---
+++ C++/sstream/sstream.c
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int main() {
+  std::stringstream ss, ss1;
+  ss << "this is a test about sstream" << 20191122 << "!";
+  std::string s = ss.str();
+  std::cout << s << std::endl;
+	
+	//this set str as the content of the stringstream of ss1
+  ss1.str(s);
+  for (int i = 0; i < 6; i++) {
+    std::string s1;
+    ss1 >> s1;
+    std::cout << s1 << std::endl;
+  }
+
+	std::cout << "stringstream trans to string is :"<<ss1.str() << std::endl;
+  std::cerr << "cerr stringstream trans to string is :"<<ss1.str() << std::endl;
+  std::clog << "clog stringstream trans to string is :"<<ss1.str() << std::endl;
+
+  return 0;
+}
