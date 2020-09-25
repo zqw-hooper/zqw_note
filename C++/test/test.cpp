a73d4916d401d7bfa0071921922656f61ada4bdb
zqw-hooper <zqw.hooper@gmail.com>
2020-09-25 08:11:52 +0800

    add makefile project

---
+++ C++/test/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+
+int main()
+{
+    std::cout << "print here!" <<std::endl;
+}
